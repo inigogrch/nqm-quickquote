4e2dd357f80ade81b63d6fca493df4a86fe6e387
Emilio Gentolia <emilio.gentolia@cybersoftbpo.com>
2025-10-02 22:23:56 +0800

    feat(summary): update summary page details based on selected loan from dashboard

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_CONFIG } from "../src/lib/config/credentials";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = SUPABASE_CONFIG.URL;
+const supabaseAnonKey = SUPABASE_CONFIG.ANON_KEY;
 
 export default createClient(
   supabaseUrl,
